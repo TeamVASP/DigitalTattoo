9473864c47b60dba1d9112d45f0787c16ace7ea5
TeamVASP <148340788+TeamVASP@users.noreply.github.com>
2023-10-18 17:06:21 +0530

    script.js

--- script.js
+++ script.js
@@ -1,27 +1,62 @@
-document.addEventListener('DOMContentLoaded', async () => {
-    await loadModel();
-});
-
-let model;
-
-async function loadModel() {
-    model = await mobilenet.load();
+body {
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    background: linear-gradient(to bottom, #251e1e, #221d1d, #1e1b1c, #1b191b, #171617);
+    color: #fff;
+    text-align: center;
+    padding: 20px;
+    margin: 0;
 }
 
-function generateTattoo() {
-    const style = document.getElementById('style').value;
-    const size = document.getElementById('size').value;
+.container {
+    max-width: 800px;
+    margin: 0 auto;
+}
 
-    // Here, you would typically use user preferences to guide the model and generate an image.
-    // This is a placeholder for demonstration purposes.
+header {
+    margin-bottom: 20px;
+}
 
-    const placeholderImageUrl = 'https://placekitten.com/300/300';
+header h1 {
+    font-size: 2.5em;
+    margin-bottom: 5px;
+}
 
-    const generatedTattooContainer = document.getElementById('generatedTattoo');
-    generatedTattooContainer.innerHTML = '';
+header p {
+    font-size: 1.2em;
+}
 
-    const tattooImage = document.createElement('img');
-    tattooImage.src = placeholderImageUrl;
-    tattooImage.alt = 'Generated Tattoo';
-    generatedTattooContainer.appendChild(tattooImage);
+#options {
+    margin-bottom: 20px;
+}
+
+#generatedTattoo img {
+    max-width: 100%;
+    margin-top: 20px;
+}
+
+label {
+    display: block;
+    margin-bottom: 5px;
+}
+
+select, textarea {
+    width: 100%;
+    padding: 8px;
+    margin-bottom: 10px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    box-sizing: border-box;
+}
+
+button {
+    background-color: #4caf50;
+    color: #fff;
+    padding: 10px 20px;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+}
+
+button:hover {
+    background-color: #45a049;
 }
